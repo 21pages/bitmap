1e04be9018c8970cbabfa46475003d89cde86d5c
21pages <2549299455@qq.com>
2019-10-18 18:01:37 +0800

    commit tmp

--- widget.cpp
+++ widget.cpp
@@ -33,44 +33,149 @@ Widget::~Widget()
 
 }
 
+#define D 32
+#define R 16
+
 void Widget::paintEvent(QPaintEvent *)
 {
     static int op = 0;
     QImage image_all(160,160,QImage::Format_Mono);
-    QPainter painter(&image_all);
+    QPainter painter;
+    painter.begin(&image_all);
     painter.fillRect(image_all.rect(),QBrush(QColor("#ffffff")));
-    QPixmap set;
-    set.load(":/pic/set.png");
-    painter.drawPixmap(QRect(16,16,32,32),set,set.rect());
-    QPixmap calendar;
-    calendar.load(":/pic/calendar.png");
-    painter.drawPixmap(QRect(112,16,32,32),calendar,calendar.rect());
+//    QPixmap set;
+//    set.load(":/pic/set.png");
+//    painter.drawPixmap(QRect(R,D,D,D),set,set.rect());
 
-    QImage image_board(320,320,QImage::Format_Mono);
+    //1
     painter.end();
+    QImage image_set(D,D,QImage::Format_Mono);
+    painter.begin(&image_set);
+    QPen p = painter.pen();
+    p.setBrush(QColor("#000000"));
+    p.setWidth(2);
+    painter.setPen(p);
+    painter.fillRect(image_set.rect(),QBrush(QColor("#ffffff")));
+    painter.setRenderHint(QPainter::Antialiasing,true);
+    painter.translate(R,R);
+    for(int i = 0; i< 16;i++) {
+        if(i%2 == 0) {
+            QPainterPath path;
+            path.moveTo(-2,10);
+            path.lineTo(-2,14);
+            path.lineTo(2,14);
+            path.lineTo(2,10);
+            painter.drawPath(path);
+        }else {
+            painter.drawArc(QRect(QPoint(-10,-10),QPoint(10,10)),(90+23)*16,10*16);
+            painter.rotate(/*22*/45);
+        }
+    }
+    painter.drawEllipse(QPoint(0,0),4,4);
+    painter.end();
+    painter.begin(&image_all);
+    painter.drawImage(QRect(R,D,D,D),image_set);
+    painter.end();
+
+
+    //2
+    QImage image_board(D,D,QImage::Format_Mono);
     painter.begin(&image_board);
     QPen pen = painter.pen();
     pen.setBrush(QColor("#000000"));
     pen.setWidth(2);
     painter.setPen(pen);
-    painter.setBrush(QColor("#000000"));
+    painter.setRenderHint(QPainter::Antialiasing,true);
+    painter.fillRect(image_board.rect(),QBrush(QColor("#ffffff")));
+    painter.translate(R,R);
+    painter.rotate(45);
     static const QPoint points[5] = {
-          QPoint(110,240),
-          QPoint(210,240),
-          QPoint(210, 100),
-          QPoint(160, 50),
-          QPoint(110, 100),
+          QPoint(-(R-8), -(R-10)),
+          QPoint(-(R-8),R-5),
+          QPoint(R-8,R-5),
+          QPoint(R-8, -(R-10)),
+          QPoint(0, -(R-2)),
       };
     painter.drawPolygon(points,5);
+    painter.drawLine(QPoint(-(R-12),2),QPoint(R-12,2));
+    painter.drawLine(QPoint(-(R-12),5),QPoint(R-12,5));
+    painter.drawLine(QPoint(-(R-12),-1),QPoint(R-12,-1));
+    //painter.drawPoint(0,-(R-8));
+    painter.drawEllipse(0,-(R-8),1,1);
     painter.end();
-    painter.begin(this);
-    QPixmap pixmap_board = QPixmap::fromImage(image_board, Qt::MonoOnly);
-    //painter.drawImage(QRect(64,16,32,32),image_board);
-    pixmap_board = pixmap_board.scaled(32,32);
-    painter.drawPixmap(QRect(64,16,32,32), pixmap_board, pixmap_board.rect());
+    painter.begin(&image_all);
+    painter.drawImage(QRect(2*D,D,D,D),image_board);
+    painter.end();
+
+    //3
+    painter.begin(&image_all);
+    QPixmap calendar;
+    calendar.load(":/pic/calendar.png");
+    painter.drawPixmap(QRect(160-R-D,D,D,D),calendar,calendar.rect());
+    painter.end();
+
+    //4
+    QImage image_db(D,D,QImage::Format_Mono);
+    painter.begin(&image_db);
+    pen = painter.pen();
+    pen.setBrush(QColor("#000000"));
+    pen.setWidth(1);
+    painter.setPen(pen);
+//    painter.setRenderHint(QPainter::Antialiasing,true);
+    painter.fillRect(image_db.rect(),QBrush(QColor("#ffffff")));
+    painter.translate(R,R);
+    painter.drawLine(QPoint(-(R-5),-(R-7)),QPoint(-(R-5),R-7));
+    painter.drawLine(QPoint(R-5,-(R-7)),QPoint(R-5,R-7));
+    painter.drawEllipse(QPoint(0,-(R-7)),R-5, 5);
+    painter.drawArc(QRect(-(R-5),R-12,D-10,10),180*16,180*16);
+    painter.end();
+    painter.begin(&image_all);
+    painter.drawImage(QRect(R,D*3,D,D),image_db);
+    painter.end();
+
+    //5
+    QImage image_pc(D,D,QImage::Format_Mono);
+    painter.begin(&image_pc);
+    pen = painter.pen();
+    pen.setBrush(QColor("#000000"));
+    pen.setWidth(6);
+    painter.setPen(pen);
+    painter.setRenderHint(QPainter::Antialiasing,true);
+    painter.fillRect(image_pc.rect(),QBrush(QColor("#ffffff")));
+    painter.translate(R,0);
+    painter.drawRect(-(R-6),6,D-12,12);
+    painter.drawLine(QPoint(-2,18),QPoint(-2,27));
+    painter.drawLine(QPoint(2,18),QPoint(2,27));
+    painter.drawLine(QPoint(-(R-4),27),QPoint(R-4,27));
+    pen = painter.pen();
+    pen.setWidth(2);
+    pen.setBrush(QColor("#ffffff"));
+    painter.setPen(pen);
+    painter.drawEllipse(QPoint(0,25),1,1);
+
+    painter.end();
+    painter.begin(&image_all);
+    painter.drawImage(QRect(2*D,D*3,D,D),image_pc);
+    painter.end();
 
 
+    //6
+    QImage image_user(D,D,QImage::Format_Mono);
+    painter.begin(&image_user);
+    painter.setRenderHint(QPainter::Antialiasing,true);
+    painter.fillRect(image_user.rect(),QBrush(QColor("#ffffff")));
+//    QPixmap pixmap_user(":/pic/user1.png");
+//    painter.drawPixmap(QRect(2,8,R-2,D-14),pixmap_user,pixmap_user.rect());
+    QPixmap pixmap_user(":/pic/user3.bmp");
+    painter.drawPixmap(image_user.rect(),pixmap_user,pixmap_user.rect());
+    image_user.save("user_tmp.bmp");
     painter.end();
+    painter.begin(&image_all);
+    painter.drawImage(QRect(160-R-D,D*3,D,D),image_user);
+    painter.end();
+
+
+    //all
     painter.begin(this);
     painter.drawImage(QRect(0,0,160,160),image_all,image_all.rect());
 
@@ -80,16 +185,19 @@ void Widget::paintEvent(QPaintEvent *)
         bmp.save("screen.bmp");
         QImage image_all2("screen.bmp");
         qDebug()<<"depth:"<<image_all2.depth();
-        const uchar *bits = image_all2.constBits();
+        QFile file_bits("bits.txt");
+        file_bits.open(QIODevice::WriteOnly |QIODevice::Text | QIODevice::Truncate);
+        QTextStream stream_bits(&file_bits);
         for(int y = 0; y<160; y++) {
             for(int x = 0; x < 160; x++) {
-                std::cout<<image_all2.pixelIndex(x, y);
+                stream_bits<<image_all2.pixelIndex(x, y);
             }
-            std::cout <<std::endl;
+            stream_bits <<"\n";
         }
-        QFile file("frameBuffer.txt");
-        file.open(QIODevice::WriteOnly |QIODevice::Text |QIODevice::Truncate);
-        QTextStream stream(&file);
+        file_bits.close();
+        QFile file_buffer("frameBuffer.txt");
+        file_buffer.open(QIODevice::WriteOnly |QIODevice::Text |QIODevice::Truncate);
+        QTextStream stream_buffer(&file_buffer);
         for(int y = 0; y<160; y++) {
             for(int x = 0; x < 20; x++) {
                 quint8 c = 0x00;
@@ -97,11 +205,11 @@ void Widget::paintEvent(QPaintEvent *)
                     c |= (image_all2.pixelIndex(x*8+i, y) << (7-i));
                 }
                QString strbyte = QString::asprintf("0x%02x",c);
-                stream<<strbyte<<",";
+                stream_buffer<<strbyte<<",";
             }
-            stream<<"\n";
+            stream_buffer<<"\n";
         }
-        file.close();
+        file_buffer.close();
 
     }
 }
